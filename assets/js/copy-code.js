599ad564e290ec7673d40faf8e7c637297898ca1
coolzest <coolzest@qq.com>
2025-09-21 17:31:32 +0800

    1

--- assets/js/copy-code.js
+++ assets/js/copy-code.js
@@ -6,18 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
     codeBlocks.forEach((block) => {
         // Create a copy button
         const button = document.createElement("button");
-        button.innerText = "复制";
+        
         button.className = "copy-button";
 
         // Append the button to the code block
         const pre = block.parentNode;
         pre.style.position = "relative";
         pre.appendChild(button);
-
         // Add click event to copy the code
         button.addEventListener("click", () => {
             navigator.clipboard.writeText(block.innerText).then(() => {
-                button.innerText = "已复制!";
                 setTimeout(() => (button.innerText = "复制"), 2000);
             });
         });
